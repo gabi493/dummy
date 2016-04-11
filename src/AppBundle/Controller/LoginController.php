c8a777c5dce007d6764d5a314e8493420bbfa1ef
Gabriel C.- <gabrielcarrillolopez@gmail.com>
2016-04-11 10:08:43 +0200

    TFG
    Login view

--- src/AppBundle/Controller/LoginController.php
+++ src/AppBundle/Controller/LoginController.php
@@ -2,22 +2,27 @@
 // src/AppBundle/Controller/LoginController.php
 namespace AppBundle\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class LoginController
+class LoginController extends Controller
 {
     public function indexAction() {
         /*return new Response(
             http_redirect("http://localhost:8000/login")
         );*/
-        $names = array("Raúl", "Jose", "Gabriel", "Araceli", "Aureli");
-        $number = rand(0, 100)%5;
 
-        return new Response(
-            '<html><body>Lucky team member: '.$names[$number].'</body></html>'
-        );
-//        return $this->redirectToRoute('login');
+        /*$number = rand(0, 6);
+
+        if ($number % 2) {*/
+            //return $this->redirect($this->generateUrl('login'));
+            return $this->render(
+                'login/form.html.twig'
+            );
+        /*}
+        else {
+            return $this->redirect($this->generateUrl('xlogin'));
+        }*/
     }
     public function nameAction()
     {
@@ -35,4 +40,10 @@ class LoginController
                 '<html><body>Error (we are talking about your life)</body></html>'
             );
         }
+    public function xloginAction()
+    {
+        return new Response(
+            '<html><body>_.-·xxXxx·-._</body></html>'
+        );
+    }
 }
