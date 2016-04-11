8e157943a830d88f60719ff7cfcf8a4fd11b2e20
Gabriel C.- <gabrielcarrillolopez@gmail.com>
2016-04-11 13:53:58 +0200

    TFG
    Login 2

--- src/AppBundle/Controller/LoginController.php
+++ src/AppBundle/Controller/LoginController.php
@@ -2,12 +2,18 @@
 // src/AppBundle/Controller/LoginController.php
 namespace AppBundle\Controller;
 
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+use AppBundle\Entity\User;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
 class LoginController extends Controller
 {
-    public function indexAction() {
+//    public function indexAction() {
         /*return new Response(
             http_redirect("http://localhost:8000/login")
         );*/
@@ -16,14 +22,55 @@ class LoginController extends Controller
 
         if ($number % 2) {*/
             //return $this->redirect($this->generateUrl('login'));
-            return $this->render(
-                'login/form.html.twig'
-            );
+//            return $this->render(
+//                'login/form.html.twig'
+//            );
         /*}
         else {
             return $this->redirect($this->generateUrl('xlogin'));
         }*/
+//    }
+
+    public function indexAction(Request $request)
+    {
+        // create a task and give it some dummy data for this example
+        $user = new User();
+
+        $form = $this->createFormBuilder($user)
+            ->add('username', TextType::class)
+            ->add('password', PasswordType::class)
+            ->add('save', SubmitType::class, array('label' => 'Login'))
+            ->add('createUser', SubmitType::class, array('label' => 'Create new user'))
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // ... perform some action, such as saving the task to the database
+            if ($form->get('save')->isClicked()) {
+                return $this->redirectToRoute('login');
+            }
+            else if ($form->get('createUser')->isClicked()) {
+                return $this->redirectToRoute('xlogin');
+            }
+
+        }
+
+
+        return $this->render('login/form.html.twig', array(
+            'form' => $form->createView(),
+        ));
     }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'error_mapping' => array(
+                'matchingUsernameAndPassword' => 'username',
+            ),
+        ));
+    }
+
     public function nameAction()
     {
         $names = array("Raúl", "Jose", "Gabriel", "Araceli", "Aureli");
