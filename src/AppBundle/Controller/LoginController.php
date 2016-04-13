776e5ba8c2a9d7082c6279477c661fe8b840d64f
Gabriel C.- <gabrielcarrillolopez@gmail.com>
2016-04-13 09:32:00 +0200

    TFG
    Login simple

--- src/AppBundle/Controller/LoginController.php
+++ src/AppBundle/Controller/LoginController.php
@@ -2,83 +2,110 @@
 // src/AppBundle/Controller/LoginController.php
 namespace AppBundle\Controller;
 
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-
 use AppBundle\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Validator\Constraints\EmailValidator;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+
+
+
 
 class LoginController extends Controller
 {
-//    public function indexAction() {
-        /*return new Response(
-            http_redirect("http://localhost:8000/login")
-        );*/
-
-        /*$number = rand(0, 6);
-
-        if ($number % 2) {*/
-            //return $this->redirect($this->generateUrl('login'));
-//            return $this->render(
-//                'login/form.html.twig'
-//            );
-        /*}
-        else {
-            return $this->redirect($this->generateUrl('xlogin'));
-        }*/
-//    }
 
     public function indexAction(Request $request)
     {
         // create a task and give it some dummy data for this example
-        //$user = new User();
-        $user = $this->getUser();
+        $user = new User();
 
         $form = $this->createFormBuilder($user)
             ->add('username', TextType::class)
             ->add('password', PasswordType::class)
             ->add('enter', SubmitType::class, array('label' => 'Login'))
-            ->add('createUser', SubmitType::class, array('label' => 'I am new here'))
+            ->add('createUser', SubmitType::class, array('label' => 'New user, here!'))
             ->getForm();
 
+        //authorAction($user);
+
         $form->handleRequest($request);
+        /*return new Response(
+            '<html>
+                    <body>
+                        <pre>' . print_r($form, true) . '</pre>
+                    </body>
+                </html>'
+        );*/
 
         if ($form->isSubmitted() && $form->isValid()) {
+            /*return new Response(
+                '<html>
+                    <body>
+                        <pre>' . print_r($form, true) . '</pre>
+                    </body>
+                </html>'
+            );*/
             // ... perform some action, such as saving the task to the database
             if ($form->get('enter')->isClicked()) {
                 return $this->redirectToRoute('login');
-            }
-            else if ($form->get('createUser')->isClicked()) {
-            /*    $em = $this->getDoctrine()->getManager();
-                $em->persist($form->get('username'));
-                $em->flush();
-            */    return $this->redirectToRoute('xlogin');
+            } else if ($form->get('createUser')->isClicked()) {
+                /*    $em = $this->getDoctrine()->getManager();
+                    $em->persist($form->get('username'));
+                    $em->flush();
+                */
+                return $this->redirectToRoute('newlogin');
             }
 
         }
-
-
         return $this->render('login/form.html.twig', array(
             'form' => $form->createView(),
         ));
     }
 
-    public function configureOptions(OptionsResolver $resolver)
-    {
+
+    public function authorAction($user) {
+        $user = new User();
+
+        // ... do something to the $user object
+
+        $validator = $this->get('validator');
+        $errors = $validator->validate($user);
+
+        if (count($errors) > 0) {
+            /*
+             * Uses a __toString method on the $errors variable which is a
+             * ConstraintViolationList object. This gives us a nice string
+             * for debugging.
+             */
+            $errorsString = (string) $errors;
+
+            /*
+             * You could also pass the collection of errors into a template:
+             * return $this->render('author/validation.html.twig',
+             * array('errors' => $errors,
+             * ));
+             *
+             * */
+            return new Response($errorsString);
+        }
+        return new Response('The author is valid! Yes!');
+    }
+
+
+    /*public function configureOptions(OptionsResolver $resolver) {
         $resolver->setDefaults(array(
             'error_mapping' => array(
                 'matchingUsernameAndPassword' => 'username',
             ),
         ));
-    }
+    }*/
 
-    public function nameAction()
-    {
+
+    public function nameAction() {
         $names = array("Raúl", "Jose", "Gabriel", "Araceli", "Aureli");
         $number = rand(0, 100)%5;
 
@@ -87,16 +114,17 @@ class LoginController extends Controller
         );
     }
 
-    public function errorAction()
-        {
+
+    public function errorAction() {
             return new Response(
                 '<html><body>Error (we are talking about your life)</body></html>'
             );
-        }
-    public function xloginAction(Request $request)
-    {
+    }
+
+
+    public function newloginAction(Request $request) {
         $user = new User();
-        $name = $request->get('username');
+        //$name = $request->get('username');
 
         $submit = $this->createFormBuilder($user)
             ->add('mail', EmailType::class)
@@ -108,11 +136,11 @@ class LoginController extends Controller
 
         $submit->handleRequest($request);
 
-        if ($submit->isSubmitted() && $submit->isValid()) {
+        if ($submit->isSubmitted() && $submit->isValid() && $submit->isPasswordLegal()) {
             // ... perform some action, such as saving the task to the database
-            $em = $this->getDoctrine()->getManager();
+        /*    $em = $this->getDoctrine()->getManager();
             $em->persist($user);
-            $em->flush();
+            $em->flush();*/
             return $this->redirectToRoute('submit_successful');
         }
 
@@ -126,6 +154,10 @@ class LoginController extends Controller
         );
 */    }
 
+    public function isPasswordLegal() {
+        return $this->firstName !== $this->password;
+    }
+
     public function submittedAction() {
         return new Response(
             '<html>
@@ -134,7 +166,7 @@ class LoginController extends Controller
                     SUCCESSFULLY SUBMITTED
                     </h1>
                     <h2>Check your e-mail to confirm your login</h2>
-                    <h3><a href="LoginController.php">Go to login page</a></h3>
+                    <h3><a href="/">Go to login page</a></h3>
                 </body>
              </html>'
         );
